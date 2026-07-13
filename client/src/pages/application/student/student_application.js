ab272c40893948e054511df37ccf4717d600595f
Jonatan Schroeder <jonatan@yorku.ca>
2026-07-13 12:03:48 -0400

    Don't show course explanation for unit 2 (#21)

--- client/src/pages/application/student/student_application.js
+++ client/src/pages/application/student/student_application.js
@@ -320,113 +320,118 @@ const StudentApplication = () => {
       />
       <p />
       <h3>Course Preferences</h3>
-      <Alert severity="info">
-        <p>
-          Note that this list is provided to gather your general interests.
-          There is no guarantee that all these courses will hire TAs, and there
-          is no guarantee we will be able to provide you with your first
-          choices. so you are strongly encouraged to list as many courses as
-          possible, preferably including all courses you qualify for.
-        </p>
-        <details open>
-          <summary>Legend for Interest and Qualification columns</summary>
-          <ul>
-            {[
-              [
-                5,
-                <>
-                  <strong>Preferred option</strong>, very interested and
-                  qualified. This is the assignment that you are most excited
-                  about and confident in your ability to perform well. You have
-                  the relevant skills, experience and knowledge to meet the
-                  expectations and requirements of the assignment. You would
-                  accept an offer for this assignment without hesitation.
-                </>,
-              ],
-              [
-                4,
-                <>
-                  <strong>Strong option</strong>, interested and qualified. This
-                  is an assignment that you are enthusiastic about and capable
-                  of doing well. You have most of the skills, experience and
-                  knowledge needed for the assignment, or you are willing to
-                  learn them quickly. You would accept an offer for this
-                  assignment.
-                </>,
-              ],
-              [
-                3,
-                <>
-                  <strong>Good option</strong>, somewhat interested and
-                  qualified. This is an assignment that you are curious about
-                  and competent in doing. You have some of the skills,
-                  experience and knowledge required for the assignment, or you
-                  are open to acquiring them. You would accept an offer for this
-                  assignment if options with higher preference are not
-                  available.
-                </>,
-              ],
-              [
-                2,
-                <>
-                  <strong>Weak option</strong>, not very interested or
-                  qualified. This is an assignment that you are indifferent
-                  about or unsure of your ability to do well. You have few of
-                  the skills, experience and knowledge necessary for the
-                  assignment, or you are reluctant to learn them. You would
-                  accept an offer for this assignment provided there are no
-                  better options available.
-                </>,
-              ],
-              [
-                1,
-                <>
-                  <strong>Not an option</strong>, not interested at all, will
-                  not accept an offer for it. This is an assignment that you are
-                  not interested in or confident in your ability to do well. You
-                  have none of the skills, experience or knowledge relevant for
-                  the assignment, or you are opposed to learning them. You would
-                  reject an offer for this assignment regardless of the
-                  situation. Choosing this option may affect your funding if
-                  none of the other courses you are qualified have availability.
-                </>,
-              ],
-            ].map(([val, label]) => (
-              <li key={val}>
-                <Box sx={{ display: "flex" }}>
-                  <Rating
-                    icon={<CircleIcon />}
-                    emptyIcon={<CircleOutlinedIcon />}
-                    readOnly
-                    value={val}
-                  />
-                  <Box sx={{ ml: 2 }}>{label}</Box>
-                </Box>
-              </li>
-            ))}
-          </ul>
-        </details>
-      </Alert>
       {termApp?.funding !== null ? (
-        Object.keys(appRows)?.map((key) => (
-          <div key={key}>
-            <h4>{key.charAt(0).toUpperCase() + key.slice(1)} Campus</h4>
-            <DatagridTable
-              key={key}
-              columns={columns}
-              idVarName={"code"}
-              loading={!appRows}
-              onEditStop={null}
-              processRowUpdate={updateRow}
-              rows={appRows[key] ?? []}
-              rowHeight={40}
-            />
-          </div>
-        ))
+        <>
+          <Alert severity="info">
+            <p>
+              Note that this list is provided to gather your general interests.
+              There is no guarantee that all these courses will hire TAs, and
+              there is no guarantee we will be able to provide you with your
+              first choices. so you are strongly encouraged to list as many
+              courses as possible, preferably including all courses you qualify
+              for.
+            </p>
+            <details open>
+              <summary>Legend for Interest and Qualification columns</summary>
+              <ul>
+                {[
+                  [
+                    5,
+                    <>
+                      <strong>Preferred option</strong>, very interested and
+                      qualified. This is the assignment that you are most
+                      excited about and confident in your ability to perform
+                      well. You have the relevant skills, experience and
+                      knowledge to meet the expectations and requirements of the
+                      assignment. You would accept an offer for this assignment
+                      without hesitation.
+                    </>,
+                  ],
+                  [
+                    4,
+                    <>
+                      <strong>Strong option</strong>, interested and qualified.
+                      This is an assignment that you are enthusiastic about and
+                      capable of doing well. You have most of the skills,
+                      experience and knowledge needed for the assignment, or you
+                      are willing to learn them quickly. You would accept an
+                      offer for this assignment.
+                    </>,
+                  ],
+                  [
+                    3,
+                    <>
+                      <strong>Good option</strong>, somewhat interested and
+                      qualified. This is an assignment that you are curious
+                      about and competent in doing. You have some of the skills,
+                      experience and knowledge required for the assignment, or
+                      you are open to acquiring them. You would accept an offer
+                      for this assignment if options with higher preference are
+                      not available.
+                    </>,
+                  ],
+                  [
+                    2,
+                    <>
+                      <strong>Weak option</strong>, not very interested or
+                      qualified. This is an assignment that you are indifferent
+                      about or unsure of your ability to do well. You have few
+                      of the skills, experience and knowledge necessary for the
+                      assignment, or you are reluctant to learn them. You would
+                      accept an offer for this assignment provided there are no
+                      better options available.
+                    </>,
+                  ],
+                  [
+                    1,
+                    <>
+                      <strong>Not an option</strong>, not interested at all,
+                      will not accept an offer for it. This is an assignment
+                      that you are not interested in or confident in your
+                      ability to do well. You have none of the skills,
+                      experience or knowledge relevant for the assignment, or
+                      you are opposed to learning them. You would reject an
+                      offer for this assignment regardless of the situation.
+                      Choosing this option may affect your funding if none of
+                      the other courses you are qualified have availability.
+                    </>,
+                  ],
+                ].map(([val, label]) => (
+                  <li key={val}>
+                    <Box sx={{ display: "flex" }}>
+                      <Rating
+                        icon={<CircleIcon />}
+                        emptyIcon={<CircleOutlinedIcon />}
+                        readOnly
+                        value={val}
+                      />
+                      <Box sx={{ ml: 2 }}>{label}</Box>
+                    </Box>
+                  </li>
+                ))}
+              </ul>
+            </details>
+          </Alert>
+          {Object.keys(appRows)?.map((key) => (
+            <div key={key}>
+              <h4>{key.charAt(0).toUpperCase() + key.slice(1)} Campus</h4>
+              <DatagridTable
+                key={key}
+                columns={columns}
+                idVarName={"code"}
+                loading={!appRows}
+                onEditStop={null}
+                processRowUpdate={updateRow}
+                rows={appRows[key] ?? []}
+                rowHeight={40}
+              />
+            </div>
+          ))}
+        </>
       ) : (
         <Alert severity="info">
           TA preferences for applicants without funding information must be
-          provided through the TA application process at CUPE info. Please make
+          provided through the TA application process at CUPE jobs. Please make
           sure to include all relevant information in that application or in
           your CV.
         </Alert>
